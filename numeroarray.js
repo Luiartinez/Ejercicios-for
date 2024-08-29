53e4e0e2c6b8cde4512adbddbf2937f7d9523d91
Luiartinez <luismartinez13hernandez@gmail.com>
2024-08-28 22:13:30 -0600

    Add files via upload

---
+++ numeroarray.js
@@ -0,0 +1,13 @@
+
+let numeros = [5, 9, 10, 12];
+
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];
+}
+
+
+let media = suma / numeros.length;
+
+console.log("La media es " + media);
---
+++ numerosparess.js
@@ -0,0 +1,5 @@
+let numero =9;
+
+for(let i=0; i < numero; i += 2 ) {
+    console.log(i)
+}
